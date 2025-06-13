4ba6159e206fbebfb9d955edd6f88d12012e6268
dracayaki <dracayaki@gmail.com>
2025-06-13 23:51:00 +0200

    login creado

--- src/main/java/controller/LoginController.java
+++ src/main/java/controller/LoginController.java
@@ -1,4 +1,42 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+
 public class LoginController {
+    public Label titleLbl;
+    public VBox loginPane;
+
+    @FXML
+    private Button loginBtn;
+
+    @FXML
+    private PasswordField passwordTxt;
+
+    @FXML
+    private Hyperlink registerLink;
+
+    @FXML
+    private TextField usernameTxt;
+
+    public void initialize() {
+        loginPane.setStyle("-fx-background-color: rgba(255, 255, 255, 0.15); -fx-background-radius: 10; -fx-padding: 20;");
+
+        // Aplicar desenfoque real
+        javafx.scene.effect.GaussianBlur blur = new javafx.scene.effect.GaussianBlur(1);
+        loginPane.setEffect(blur);
+    }
+
+    @FXML
+    void handleLogin(ActionEvent event) {
+
+    }
+
+    @FXML
+    void registerAction(ActionEvent event) {
+
+    }
 }
--- src/main/java/controller/LogoController.java
+++ src/main/java/controller/LogoController.java
@@ -1,7 +1,28 @@
 package controller;
 
+import javafx.scene.control.Label;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
 
 public class LogoController {
     public Pane main_logo_pane;
+    public Label lblTitle2;
+    public Label lblTitle;
+
+    public void initialize() {
+        main_logo_pane.setFocusTraversable(true); //indica que puede recibir el foco del teclado
+        main_logo_pane.requestFocus(); //sin esto no capta las teclas
+    }
+
+    public void handleKeyPressed(KeyEvent event) {
+        if (event.getCode() == KeyCode.SPACE){
+            try {
+                System.out.println("should open");
+                Main.setScene("/view/fxml/login.fxml", "Login");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
