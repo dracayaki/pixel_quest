ec9c072178add4e81f74d92ef835ed7a3f883019
dracayaki <dracayaki@gmail.com>
2025-06-13 23:54:14 +0200

    login creado

---
+++ src/main/java/controller/Main.java
@@ -0,0 +1,35 @@
+package controller;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+
+    private static Stage primaryStage;
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        primaryStage = stage;
+        setScene("/view/fxml/main.fxml", "Pixel Quest");
+    }
+
+    public static void setScene(String fxmlName, String title) throws IOException {
+        FXMLLoader loader = new FXMLLoader(Main.class.getResource(fxmlName));
+        Scene scene = new Scene(loader.load(), 800, 600);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle(title);
+        primaryStage.setResizable(false);
+        primaryStage.show();
+
+        scene.getRoot().requestFocus(); // asi la ventana puede detectar las teclas desde el primer momento
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
