ee0d4a3f510d7ca5833c6d6a051b52cd970e67b6
dracayaki <dracayaki@gmail.com>
2025-06-15 02:10:54 +0200

    snake moving lets fucking go

--- src/main/java/controller/DeadController.java
+++ src/main/java/controller/DeadController.java
@@ -1,4 +1,31 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import model.Object.Session;
+
+import java.io.IOException;
+
 public class DeadController {
+    public Button retryBtn;
+    public ImageView deathImage;
+    public Button menuBtn;
+
+    public void handleRetry(ActionEvent actionEvent) {
+        try {
+            int currentLevel = Session.getCurrentLevel(); // nivel actual guardado
+            Main.setScene("/view/fxml/level.fxml", "Level " + currentLevel);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void handleMenu(ActionEvent actionEvent) {
+        try {
+            Main.setScene("/view/fxml/levelMenu.fxml", "Level Selection");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/controller/LevelController.java
+++ src/main/java/controller/LevelController.java
@@ -3,6 +3,7 @@ package controller;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -11,7 +12,12 @@ import model.DAO.LevelDAO;
 import model.Object.Level;
 import model.Object.Session;
 
+
+import java.io.IOException;
 import java.net.URL;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class LevelController implements Initializable {
@@ -20,6 +26,12 @@ public class LevelController implements Initializable {
     public Label timerLbl;
     private int currentLevel;
     private static final int TILE_SIZE = 50;
+    private static final int TIME_LIMIT = 120;
+    private Thread timerThread;
+    private int snakeRow;
+    private int snakeCol;
+    private List<ImageView> snakeBody = new ArrayList<>();
+    private String[][] map;
 
     public void setLevel(int level) {
         this.currentLevel = level;
@@ -35,11 +47,25 @@ public class LevelController implements Initializable {
         Level level = levelDAO.getLevelByNumber(currentLevelNumber);
 
         if (level != null) {
-            String[][] map = level.createMap();
+            map = level.createMap();
             drawMap(map);
             startTimer();
 
         }
+
+        levelGrid.setFocusTraversable(true);
+        levelGrid.requestFocus();
+
+        Platform.runLater(() -> {
+            levelGrid.getScene().setOnKeyPressed(event -> {
+                switch (event.getCode()) {
+                    case UP -> moveSnake(-1, 0);
+                    case DOWN -> moveSnake(1, 0);
+                    case LEFT -> moveSnake(0, -1);
+                    case RIGHT -> moveSnake(0, 1);
+                }
+            });
+        });
     }
 
     private void drawMap(String[][] map) {
@@ -57,9 +83,6 @@ public class LevelController implements Initializable {
                     case "C":
                         tileImage.setImage(new Image("/img/coin.png"));
                         break;
-                    case "P":
-                        tileImage.setImage(new Image("/img/snake.jpg"));
-                        break;
                     case "E":
                     default:
                         continue;
@@ -73,24 +96,97 @@ public class LevelController implements Initializable {
 
             }
         }
+
+        snakeRow = map.length - 3;
+        snakeCol = 2;
+
+        ImageView head = createSnakeBody();
+        levelGrid.add(head, snakeCol, snakeRow);
+        snakeBody.add(head);
     }
 
     private void startTimer() {
         Runnable task = () -> {
-            int seconds = 0;
-            while (true) {
+            int seconds = TIME_LIMIT;
+
+            while (seconds >= 0) {
                 int finalSeconds = seconds;
-                Platform.runLater(() -> timerLbl.setText("Time: " + (finalSeconds / 60) + ":" + String.format("%02d", finalSeconds % 60)));
+                Platform.runLater(() ->
+                        timerLbl.setText("Time: " + (finalSeconds / 60) + ":" + String.format("%02d", finalSeconds % 60))
+                );
                 try {
                     Thread.sleep(1000);
                 } catch (InterruptedException e) {
                     break;
                 }
-                seconds++;
+                seconds--;
             }
+
+
+            Platform.runLater(() -> {
+                try {
+                    Main.setScene("/view/fxml/you_died.fxml", "You Died");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
         };
-        Thread timerThread = new Thread(task);
+
+        timerThread = new Thread(task);
         timerThread.setDaemon(true);
         timerThread.start();
+
+    }
+
+    private void moveSnake(int moveRow, int moveCol){
+
+
+        int newRow = snakeRow + moveRow;
+        int newCol = snakeCol + moveCol;
+
+        if (newRow < 0 || newRow >= levelGrid.getRowCount() || newCol < 0 || newCol >= levelGrid.getColumnCount()) {
+            return;
+        }
+
+        String targetTile = map[newRow][newCol];
+
+        if (targetTile.equals("W")) {
+            return;
+        }
+
+        if (targetTile.equals("C")) {
+            ImageView newSegment = createSnakeBody();
+            levelGrid.add(newSegment, snakeCol, snakeRow);
+            snakeBody.add(0, newSegment);
+            map[newRow][newCol] = "E";
+            removeNodeAt(newRow, newCol);
+        } else {
+            ImageView tail = snakeBody.remove(snakeBody.size() - 1);
+            GridPane.setRowIndex(tail, newRow);
+            GridPane.setColumnIndex(tail, newCol);
+            snakeBody.add(0, tail);
+        }
+
+        GridPane.setRowIndex(snakeBody.get(0), newRow);
+        GridPane.setColumnIndex(snakeBody.get(0), newCol);
+        snakeRow = newRow;
+        snakeCol = newCol;
+    }
+
+    private void removeNodeAt(int row, int col) {
+        levelGrid.getChildren().removeIf(node ->
+                GridPane.getRowIndex(node) != null &&
+                        GridPane.getColumnIndex(node) != null &&
+                        GridPane.getRowIndex(node) == row &&
+                        GridPane.getColumnIndex(node) == col
+        );
+    }
+
+    private ImageView createSnakeBody() {
+        ImageView body = new ImageView(new Image("/img/snake.jpg"));
+        body.setFitWidth(TILE_SIZE);
+        body.setFitHeight(TILE_SIZE);
+        body.setPreserveRatio(true);
+        return body;
     }
 }
